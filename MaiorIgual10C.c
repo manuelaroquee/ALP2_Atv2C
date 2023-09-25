20b60fa78ae223b3f53fba2ebbc90fe43b37d9fe
Manuela Roque <65978895+manuelaroquee@users.noreply.github.com>
2023-09-25 12:39:50 -0400

    Create MaiorIgual10C.c

---
+++ MaiorIgual10C.c
@@ -0,0 +1,16 @@
+//Programa em C que leia um número e escreva o texto “Sim – É maior ou igual   a 10” se ele for maior ou igual a 10;
+#include <stdio.h>
+
+int num;
+int main(){
+    
+    printf("Digite um número:");
+    scanf("%d", &num);
+    if (num>=10){
+        printf("Sim - É maior ou igual a 10");
+    }else {
+        printf("Não - Não é maior ou igual a 10");
+    
+    return 0;
+}
+}
